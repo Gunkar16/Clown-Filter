3101e9cd40e43a5e0e87cee704eabd444f29ca6f
Gunkar16 <76874019+Gunkar16@users.noreply.github.com>
2021-06-07 18:51:02 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-noseX = 0;
-noseY = 0;
+eye_leftX = 0;
+eye_leftY = 0;
+
+eye_rightX = 0;
+eye_rightY = 0;
+
+mouthX = 0;
+mouthY = 0;
+
+hairX = 0;
+hairY = 0;
+
 function preload(){
-    clown_nose = loadImage('nose.png')
+    clown_nose = loadImage('eyes.png');
+    mouth = loadImage('lips.png');
+    hair = loadImage('hair.png');
 }
 
 function setup(){
@@ -10,24 +22,35 @@ function setup(){
     Video = createCapture(VIDEO);
     Video.size(400,300);
     Video.hide();
+
     pose_net = ml5.poseNet(Video,modelLoaded);
-    pose_net.on('pose',gotPoses);
+    pose_net.on('pose', gotPoses);
 }
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+
 function gotPoses(result){
         if(result.length>0){
             console.log(result);
             console.log("nose x = " + result[0].pose.nose.x);
             console.log("nose y = " + result[0].pose.nose.y);
-            noseX = result[0].pose.nose.x - 15;
-            noseY = result[0].pose.nose.Y - 15;
+            eye_leftX = result[0].pose.leftEye.x - 20;
+            eye_leftY = result[0].pose.leftEye.y - 15;
+
+            eye_rightX = result[0].pose.rightEye.x - 20;
+            eye_rightY = result[0].pose.rightEye.y - 15;
+
+            mouthX = result[0].pose.nose.x;
+            mouthY = result[0].pose.nose.y;
+
+            hairX = result[0].pose.nose.x;
+            hairY = result[0].pose.nose.y;
         }
         else{
             console.log("person not detected")
         }
-}
-
-function modelLoaded(){
-    console.log("Model Loaded");
 }
 
 function take_snapshot(){
@@ -35,5 +58,16 @@ function take_snapshot(){
 }
 function draw(){
     image(Video,0,0,400,300);
-    image(clown_nose,noseX,noseY,100,100);
+    fill(0,0,0);
+    stroke(0,0,0);
+    
+    image(clown_nose , eye_leftX , eye_leftY,40,40);
+    circle(eye_leftX + 20,eye_leftY + 20,20);
+
+    image(clown_nose , eye_rightX , eye_rightY,40,40);
+    circle(eye_rightX + 20,eye_rightY + 20,20);
+
+    image(mouth,mouthX - 30,mouthY,60,50);
+
+    image(hair,hairX - 60,hairY - 130,150,100);
 }
